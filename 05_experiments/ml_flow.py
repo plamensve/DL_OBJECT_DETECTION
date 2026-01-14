9e2c3f17b8b5e140acc7169baed1a11fd8b953ea
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2026-01-14 21:01:31 +0200

    ADD: new runs, better results

--- 05_experiments/ml_flow.py
+++ 05_experiments/ml_flow.py
@@ -1,24 +1,27 @@
 import mlflow
+from mlflow.protos.databricks_uc_registry_messages_pb2 import PATH
 
 mlflow.set_tracking_uri("http://127.0.0.1:5000")
 mlflow.set_experiment("SMBT")
 
 with mlflow.start_run():
-    mlflow.log_param("epochs", 200)
+    mlflow.log_param("epochs", 100)
     mlflow.log_param("batch_size", 4)
-    mlflow.log_param("img_size", 960)
+    mlflow.log_param("img_size", 640)
     mlflow.log_param("device", 0)
     mlflow.log_param("workers", 0)
 
-    mlflow.log_artifact('../01_notebooks/runs/detect/smbt_detection4/BoxF1_curve.png')
-    mlflow.log_artifact('../01_notebooks/runs/detect/smbt_detection4/BoxP_curve.png')
-    mlflow.log_artifact('../01_notebooks/runs/detect/smbt_detection4/BoxPR_curve.png')
-    mlflow.log_artifact('../01_notebooks/runs/detect/smbt_detection4/BoxR_curve.png')
-    mlflow.log_artifact('../01_notebooks/runs/detect/smbt_detection4/confusion_matrix.png')
-    mlflow.log_artifact('../01_notebooks/runs/detect/smbt_detection4/confusion_matrix_normalized.png')
-    mlflow.log_artifact('../01_notebooks/runs/detect/smbt_detection4/labels.jpg')
-    mlflow.log_artifact('../01_notebooks/runs/detect/smbt_detection4/results.png')
+    PATH_ARTIFACTS = '../01_notebooks/runs/detect/smbt_detection62/'
 
-    mlflow.log_artifact('../01_notebooks/runs/detect/smbt_detection4/results.csv')
+    mlflow.log_artifact(f'{PATH_ARTIFACTS}BoxF1_curve.png')
+    mlflow.log_artifact(f'{PATH_ARTIFACTS}BoxP_curve.png')
+    mlflow.log_artifact(f'{PATH_ARTIFACTS}BoxPR_curve.png')
+    mlflow.log_artifact(f'{PATH_ARTIFACTS}BoxR_curve.png')
+    mlflow.log_artifact(f'{PATH_ARTIFACTS}confusion_matrix.png')
+    mlflow.log_artifact(f'{PATH_ARTIFACTS}confusion_matrix_normalized.png')
+    mlflow.log_artifact(f'{PATH_ARTIFACTS}labels.jpg')
+    mlflow.log_artifact(f'{PATH_ARTIFACTS}results.png')
 
-    mlflow.log_artifact("../01_notebooks/runs/detect/smbt_detection4/weights/best.pt")
+    mlflow.log_artifact(f'{PATH_ARTIFACTS}results.csv')
+
+    mlflow.log_artifact(f"{PATH_ARTIFACTS}weights/best.pt")
