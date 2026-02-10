1e25a847b53ee28c1384c5f758037d4c3842a26d
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2026-02-10 11:10:00 +0200

    ADD: new training

--- 05_experiments/ml_flow.py
+++ 05_experiments/ml_flow.py
@@ -2,7 +2,20 @@ import sys
 from pathlib import Path
 
 import mlflow
-from settings_files.augmentation_config import AUGMENTATION_PARAMS
+
+AUGMENTATION_PARAMS = {
+    "mosaic": 1.0,
+    "mixup": 0.1,
+    "close_mosaic": 10,
+    "fliplr": 0.5,
+    "hsv_h": 0.015,
+    "hsv_s": 0.7,
+    "hsv_v": 0.4,
+
+    "degrees": 10,
+    "shear": 2,
+    "scale": 0.5,
+}
 
 mlflow.set_tracking_uri("http://127.0.0.1:5000")
 mlflow.set_experiment("SMBT")
@@ -16,7 +29,7 @@ with mlflow.start_run():
 
     mlflow.log_params(AUGMENTATION_PARAMS)
 
-    PATH_ARTIFACTS = '../01_notebooks/runs/detect/smbt_detection_11'
+    PATH_ARTIFACTS = '../01_notebooks/runs/detect/smbt_detection_15'
 
     mlflow.log_artifact(f'{PATH_ARTIFACTS}/BoxF1_curve.png')
     mlflow.log_artifact(f'{PATH_ARTIFACTS}/BoxP_curve.png')
